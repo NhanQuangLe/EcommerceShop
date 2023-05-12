a1f051004ce73d8b47a0f84039c2e5e3bf3d1bf0
21522402 <21522402@gm.uit.edu.vn>
2023-05-12 09:19:43 +0700

    them mot ti

--- app/src/main/java/com/example/ecommerceshop/MainShopActivity.java
+++ app/src/main/java/com/example/ecommerceshop/MainShopActivity.java
@@ -9,6 +9,6 @@ public class MainShopActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_user_orders);
+        setContentView(R.layout.fragment_favourite_product);
     }
 }
