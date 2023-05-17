53c17f68b9965a800770549e9867ee8a3390e5e1
21522683 <21522683@gm.uit.edu.vn>
2023-05-17 15:35:30 +0700

    Tinh up giao dien Login SignUp

---
+++ app/src/main/java/com/example/ecommerceshop/activity_forgot_pass_code.java
@@ -0,0 +1,14 @@
+package com.example.ecommerceshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_forgot_pass_code extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass_code);
+    }
+}
