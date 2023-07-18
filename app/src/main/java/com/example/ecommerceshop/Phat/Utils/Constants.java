3d552965a3d487c415f53fde8ec49401747a5d77
Huynh Ngoc Qui <quichua333@gmail.com>
2023-07-18 23:41:59 +0700

    .

--- app/src/main/java/com/example/ecommerceshop/Phat/Utils/Constants.java
+++ app/src/main/java/com/example/ecommerceshop/Phat/Utils/Constants.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Constants {
-    public static String[] orderStatus = { "Processing","Completed","Cancelled"};
+    public static String[] orderStatus = { "Processing","Cancelled"};
     public static String[] orderStatus1 = { "Completed"};
     public static final String FCM_KEY ="AAAA3n9da1c:APA91bFiKpUQAunEUVuCbl1GZvDQVS0y0XZedv_bVsMAHVq9YIiZhaFc6kK65mRie-PHcf18vLakRlzCcLB2Fwph5w-cJgpgcjbjPD7OBRRXfWMJol-aiyC3fyutC2-ZYHnTCpa_R1ei";
     public  static  final String FCM_TOPIC ="PUSH_NOTIFICATIONS";
